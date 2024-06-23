772a127ee2d4f2ca5b7262f4a1720d5ed985bdb3
madeleinehll <hill.madeleine13@yahoo.de>
2024-06-23 17:27:00 +0200

    Karte einbauen

--- HoheTauernTourismus/main.js
+++ HoheTauernTourismus/main.js
@@ -3,11 +3,30 @@ var großglockner = {
     lat: 47.074531,
     lng: 12.6939,
     title: "Großglockner"
-  };
-  
-  // Karte initialisieren
-  var map = L.map("map").setView([großglockner.lat, großglockner.lng], 10);
-  
-  // BasemapAT Layer mit Leaflet provider plugin als startLayer Variable
-  var startLayer = L.tileLayer.provider("BasemapAT.grau");
-  startLayer.addTo(map);
+};
+
+// Karte initialisieren
+var map = L.map("map").setView([großglockner.lat, großglockner.lng], 10);
+
+// BasemapAT Layer mit Leaflet provider plugin als startLayer Variable
+var startLayer = L.tileLayer.provider("BasemapAT.grau");
+startLayer.addTo(map);
+
+var themaLayer = {
+    borders: L.featureGroup().addTo(map),
+}
+
+L.control
+    .layers({
+        "BasemapAT Grau": startLayer,
+        "BasemapAT Standard": L.tileLayer.provider("BasemapAT.basemap"),
+        "BasemapAT High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
+        "BasemapAT Gelände": L.tileLayer.provider("BasemapAT.terrain"),
+        "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
+        "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+        "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
+    },
+        {
+            "Nationalparkgrenzen": themaLayer.borders,
+        })
+    .addTo(map);
